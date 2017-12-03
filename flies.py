9e9b6a3d071ac440689001f5b4c9ddb983372b65
Oorja <ojm238@nyu.edu>
2017-12-03 11:54:25 +0400

    added class hand, incomplete

--- flies.py
+++ flies.py
@@ -1,10 +1,39 @@
 import sys
 import pygame
-import random
+import math 
+
 pygame.init()
-surface = pygame.display.set_mode((640,480))
+screen = pygame.display.set_mode((1000,800))
+mouse = pygame.mouse.get_pos()
+
+class Pair:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y 
+    def __add__(self, other):
+        new_x = self.x + other.x
+        new_y = self.y + other.y
+        return Pair(new_x, new_y)
+class Hand:
+    def __init__(self):
+        self.image = pygame.image.load("hand.png").convert_alpha()
+        self.position = Pair(mouse[0], mouse[1])
+        self.speed = 50
+        self.hitbox = pygame.Rect(mouse[0], mouse[1], 200, 293)
+    def display(self):
+        screen.blit(self.image, mouse)
+  #  def move(self):
+
+        
+hand = Hand()
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.display.quit()
             sys.exit()
+    screen.fill((0))
+    
+   # hand.move()
+    hand.display()
+    
+    pygame.display.update()
